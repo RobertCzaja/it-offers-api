92829108d1fe45858a71f210634a4bce1289d9a1
RobertCzaja <-->
2024-08-03 11:43:53 +0200

    inject repository

--- src/main/java/pl/api/itoffers/security/ui/AuthController.java
+++ src/main/java/pl/api/itoffers/security/ui/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.api.itoffers.security.application.JwtUtil;
 import pl.api.itoffers.security.application.User;
+import pl.api.itoffers.security.infrastructure.UserRepository;
 
 @RestController
 public class AuthController {
@@ -13,15 +14,17 @@ public class AuthController {
     public final static String GET_TOKEN_PATH = "/auth";
 
     private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
 
-    public AuthController(JwtUtil jwtUtil) {
+    public AuthController(JwtUtil jwtUtil, UserRepository userRepository) {
         this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
     }
 
     @GetMapping(AuthController.GET_TOKEN_PATH)
     public ResponseEntity<AuthResponse> auth()
     {
-        User user = new User("example@email.com", "admin", "John", "Doe");
+        User user = userRepository.findUserByEmail("user@email.com"); // get from request
 
         var authResponse = new AuthResponse(jwtUtil.createToken(user));
         return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
