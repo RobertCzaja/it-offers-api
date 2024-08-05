2de856b86f16c0c604e9cc6adc5c6364695c7b95
RobertCzaja <robertczaja.dev@gmail.com>
2024-08-05 18:24:23 +0200

    Cleaning

--- src/test/java/pl/api/itoffers/security/AuthControllerITest.java
+++ src/test/java/pl/api/itoffers/security/AuthControllerITest.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.util.UriBuilder;
 import pl.api.itoffers.security.ui.controller.AuthController;
 
 import java.util.HashMap;
