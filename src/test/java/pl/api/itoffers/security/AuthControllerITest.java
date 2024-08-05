9995af6d28ce3564e21aadc8e7fb0dd595dd83c4
RobertCzaja <robertczaja.dev@gmail.com>
2024-08-05 17:21:43 +0200

    Make the test a bit better

--- src/test/java/pl/api/itoffers/security/AuthControllerITest.java
+++ src/test/java/pl/api/itoffers/security/AuthControllerITest.java
@@ -1,11 +1,20 @@
 package pl.api.itoffers.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.lang.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
 import pl.api.itoffers.security.ui.controller.AuthController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.net.URI;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,11 +26,17 @@ public class AuthControllerITest {
 
     @Test
     public void shouldGetAccessToken() throws Exception {
-        String email = "a@a.pl";
-        String url = AuthController.GET_TOKEN_PATH+"?email="+email;
+        URI uri = UriComponentsBuilder
+                .fromUriString(AuthController.GET_TOKEN_PATH)
+                .queryParam("email", "a@a.pl")
+                .build()
+                .toUri();
 
-        ResponseEntity<String> response = template.getForEntity(url, String.class);
+        ResponseEntity<String> response = template.getForEntity(uri, String.class);
 
+        Map<String, Object> responseMap = new ObjectMapper().readValue(response.getBody(), HashMap.class);
+
+        Assert.notNull(responseMap.get("token"));
         assertThat(response.getStatusCode().is2xxSuccessful());
     }
 
