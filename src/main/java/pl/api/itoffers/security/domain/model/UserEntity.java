f603e61e2189c8efc8ff167543be5ea8c735874e
RobertCzaja <robertczaja.dev@gmail.com>
2024-08-07 07:46:34 +0200

    Save user entity

--- src/main/java/pl/api/itoffers/security/domain/model/UserEntity.java
+++ src/main/java/pl/api/itoffers/security/domain/model/UserEntity.java
@@ -2,12 +2,13 @@ package pl.api.itoffers.security.domain.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Data
 @Entity
-@Table(name = "user")
+@Table(name = "\"user\"") // TODO to find out why Hibernate can't generate "" by itself
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/pl/api/itoffers/security/ui/cli/CreateUserCommand.java
+++ src/main/java/pl/api/itoffers/security/ui/cli/CreateUserCommand.java
@@ -1,12 +1,22 @@
 package pl.api.itoffers.security.ui.cli;
 
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.shell.command.annotation.Option;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import pl.api.itoffers.security.domain.model.UserEntity;
 
+import java.time.LocalDate;
+
+@Transactional
 @ShellComponent
 public class CreateUserCommand {
 
-    public CreateUserCommand() {
+    private final EntityManager entityManager;
+
+    public CreateUserCommand(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
     @ShellMethod(key="create-user", value="Create user that can authenticate")
@@ -14,6 +24,13 @@ public class CreateUserCommand {
             String email,
             String password
     ) {
+        UserEntity user = new UserEntity();
+        user.setEmail(email);
+        user.setDate(LocalDate.now());
+
+        entityManager.persist(user);
+        entityManager.flush();
+
         return String.format("User %s created (%s)", email, password);
     }
 }
