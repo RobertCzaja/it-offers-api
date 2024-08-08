00068f9254d4f3f7ebf3c768b9ae0e602781b7a6
RobertCzaja <robertczaja.dev@gmail.com>
2024-08-08 07:05:27 +0200

    CSRF protection provided from Spring is no needed in case of using Stateless JWT Auth

--- src/main/java/pl/api/itoffers/security/framework/SecurityConfig.java
+++ src/main/java/pl/api/itoffers/security/framework/SecurityConfig.java
@@ -2,16 +2,20 @@ package pl.api.itoffers.security.framework;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import pl.api.itoffers.security.ui.controller.AuthController;
+import pl.api.itoffers.security.ui.controller.UserController;
 
 @Configuration
 @EnableWebSecurity
@@ -41,7 +45,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(Customizer.withDefaults())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests()
                 .requestMatchers(AuthController.GET_TOKEN_PATH)
                 .permitAll()
--- src/main/java/pl/api/itoffers/security/ui/controller/UserController.java
+++ src/main/java/pl/api/itoffers/security/ui/controller/UserController.java
@@ -1,2 +1,20 @@
-package pl.api.itoffers.security.ui.controller;public class UserController {
+package pl.api.itoffers.security.ui.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    public final static String PATH = "/user";
+
+    public UserController() {
+    }
+
+    /** TODO Return type change for specific Response object */
+    @PostMapping(UserController.PATH)
+    public String create()
+    {
+        return "User Created";
+    }
 }
