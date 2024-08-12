35cdc57d0939ff52b1bcb135eb2176cb1131518b
RobertCzaja <robertczaja.dev@gmail.com>
2024-08-12 18:34:03 +0200

    Test Auth endpoint request body payload validation

--- src/test/java/pl/api/itoffers/helper/AuthRequestBodyFactory.java
+++ src/test/java/pl/api/itoffers/helper/AuthRequestBodyFactory.java
@@ -4,12 +4,22 @@ import org.springframework.http.HttpEntity;
 import pl.api.itoffers.security.ui.request.AuthorizationRequest;
 
 public class AuthRequestBodyFactory {
+    public static final String EMAIL = "a@a.pl";
+    public static final String PASSWORD = "admin1234";
+
     public static HttpEntity<AuthorizationRequest> create()
     {
+        return create(EMAIL, PASSWORD);
+    }
+
+    public static HttpEntity<AuthorizationRequest> create(String email, String password)
+    {
         AuthorizationRequest requestBody = new AuthorizationRequest();
-        requestBody.setEmail("a@a.pl");
-        requestBody.setPassword("admin1234");
+        requestBody.setEmail(email);
+        requestBody.setPassword(password);
 
         return new HttpEntity<>(requestBody);
     }
+
+
 }
--- src/test/java/pl/api/itoffers/security/AuthControllerITest.java
+++ src/test/java/pl/api/itoffers/security/AuthControllerITest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import pl.api.itoffers.helper.AuthRequestBodyFactory;
 import pl.api.itoffers.security.ui.controller.AuthController;
@@ -19,12 +20,30 @@ public class AuthControllerITest {
     private TestRestTemplate template;
 
     @Test
-    public void shouldGetAccessToken() throws Exception {
+    public void shouldGetAccessToken() {
         ResponseEntity<AuthResponse> response = template.postForEntity(AuthController.GET_TOKEN_PATH, AuthRequestBodyFactory.create(), AuthResponse.class);
 
         assertThat(response.getBody().getToken()).isNotNull();
         assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
     }
 
-    // TODO write Request Body Payload validation tests
+    @Test
+    void shouldNotHandleInvalidEmail() {
+        ResponseEntity<AuthResponse> response = template.postForEntity(
+                AuthController.GET_TOKEN_PATH,
+                AuthRequestBodyFactory.create("invalidEmail", AuthRequestBodyFactory.PASSWORD),
+                AuthResponse.class
+        );
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    void shouldNotHandleInvalidPassword() {
+        ResponseEntity<AuthResponse> response = template.postForEntity(
+                AuthController.GET_TOKEN_PATH,
+                AuthRequestBodyFactory.create(AuthRequestBodyFactory.EMAIL, "tooLongPassword123213123123abacabca"),
+                AuthResponse.class
+        );
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
 }
