d8dad32c3f8104349518a7a704ea7658529c973f
RobertCzaja <robertczaja.dev@gmail.com>
2024-08-17 08:45:03 +0200

    Move integration tests to more appropriate package

--- src/test/java/pl/api/itoffers/integration/security/UserControllerITest.java
+++ src/test/java/pl/api/itoffers/integration/security/UserControllerITest.java
@@ -1,4 +1,4 @@
-package pl.api.itoffers.security;
+package pl.api.itoffers.integration.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
