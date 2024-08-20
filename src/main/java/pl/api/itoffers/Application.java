10e907707dcff7f96c6ecb4f123e9999e5cec6b8
RobertCzaja <robertczaja.dev@gmail.com>
2024-08-20 18:04:03 +0200

    Disable MongoDB in order to testing Github Actions pipeline

--- src/main/java/pl/api/itoffers/Application.java
+++ src/main/java/pl/api/itoffers/Application.java
@@ -6,12 +6,14 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.shell.Shell;
 import org.springframework.shell.jline.NonInteractiveShellRunner;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
