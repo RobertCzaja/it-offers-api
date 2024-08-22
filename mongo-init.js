b473aca1cd3d0dff75d7fcb1067e7b5faec131a5
RobertCzaja <robertczaja.dev@gmail.com>
2024-08-22 19:37:10 +0200

    Testing mongodb config

--- mongo-init.js
+++ mongo-init.js
@@ -10,20 +10,20 @@ db.createUser(
         ]
     }
 );
-db = db.getSiblingDB('it-offers-dev');
-db.createUser(
-    {
-        user: "admin",
-        pwd: "admin",
-        roles: [{role: "readWrite", db: "it-offers-dev"}]
-    }
-);
-
-db = db.getSiblingDB('it-offers-integration-tests');
-db.createUser(
-    {
-        user: "admin",
-        pwd: "admin",
-        roles: [{role: "readWrite",db: "it-offers-integration-tests"}]
-    }
-);
+// db = db.getSiblingDB('it-offers-dev');
+// db.createUser(
+//     {
+//         user: "admin",
+//         pwd: "admin",
+//         roles: [{role: "readWrite", db: "it-offers-dev"}]
+//     }
+// );
+//
+// db = db.getSiblingDB('it-offers-integration-tests');
+// db.createUser(
+//     {
+//         user: "admin",
+//         pwd: "admin",
+//         roles: [{role: "readWrite",db: "it-offers-integration-tests"}]
+//     }
+// );
