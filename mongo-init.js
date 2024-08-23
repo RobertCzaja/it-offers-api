1c73dda56f850ae4964737f664e8ab79722b45f5
RobertCzaja <robertczaja.dev@gmail.com>
2024-08-23 16:48:47 +0200

    MongoDB working with Integration Tests, Application context and CICD

--- mongo-init.js
+++ mongo-init.js
@@ -1,29 +1,17 @@
+db = db.getSiblingDB('it-offers-dev');
 db.createUser(
     {
         user: "admin",
         pwd: "admin",
-        roles: [
-            {
-                role: "readWrite",
-                db: "it-offers"
-            }
-        ]
+        roles: [{role: "readWrite", db: "it-offers-dev"}]
     }
 );
-// db = db.getSiblingDB('it-offers-dev');
-// db.createUser(
-//     {
-//         user: "admin",
-//         pwd: "admin",
-//         roles: [{role: "readWrite", db: "it-offers-dev"}]
-//     }
-// );
-//
-// db = db.getSiblingDB('it-offers-integration-tests');
-// db.createUser(
-//     {
-//         user: "admin",
-//         pwd: "admin",
-//         roles: [{role: "readWrite",db: "it-offers-integration-tests"}]
-//     }
-// );
+
+db = db.getSiblingDB('it-offers-integration-tests');
+db.createUser(
+    {
+        user: "admin",
+        pwd: "admin",
+        roles: [{role: "readWrite",db: "it-offers-integration-tests"}]
+    }
+);
