a23e55798ff1cb69bf71d589b18e97a831070f72
RobertCzaja <robertczaja.dev@gmail.com>
2024-09-10 19:47:41 +0200

    Remove unused dependencies

--- src/main/java/pl/api/itoffers/provider/justjoinit/ui/cli/FetchOffersCommand.java
+++ src/main/java/pl/api/itoffers/provider/justjoinit/ui/cli/FetchOffersCommand.java
@@ -1,19 +1,10 @@
 package pl.api.itoffers.provider.justjoinit.ui.cli;
 
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
-import pl.api.itoffers.offer.application.repository.TechnologyRepository;
-import pl.api.itoffers.offer.application.service.OfferService;
-import pl.api.itoffers.provider.justjoinit.JustJoinItProvider;
 import pl.api.itoffers.provider.justjoinit.service.FetchJustJoinItOffersService;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
 
 @ShellComponent
 public class FetchOffersCommand {
