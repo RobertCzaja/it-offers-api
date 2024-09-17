a8e1b1c838cf31a4aaeb7b8c788df9816d90ddc1
RobertCzaja <robertczaja.dev@gmail.com>
2024-09-17 19:10:52 +0200

    Move code to separated private methods

--- src/main/java/pl/api/itoffers/offer/application/service/ReportService.java
+++ src/main/java/pl/api/itoffers/offer/application/service/ReportService.java
@@ -13,6 +13,7 @@ import pl.api.itoffers.offer.domain.Offer;
 import java.time.LocalDateTime;
 import java.util.*;
 
+/* TODO think about refactor of that class */
 @Service
 public class ReportService {
     private final OfferRepository offerRepository;
@@ -21,84 +22,98 @@ public class ReportService {
         this.offerRepository = offerRepository;
     }
 
-    /* todo needs to be refactored */
     public CategoriesStatisticsDto computeCategoriesStatistics(Pair<LocalDateTime, LocalDateTime> period, Set<String> technologies) {
 
         HashMap<String, List<CategoryDto>> technologiesWithCategories = new HashMap<String, List<CategoryDto>>();
 
-        for (Offer offer : offerRepository.fetch()) {
+        for (Offer offer : offerRepository.fetch(/*todo pass arguments here*/)) {
             List<CategoryDto> technologyWithCategories = technologiesWithCategories.get(offer.getTechnology());
 
             if (null == technologyWithCategories) {
-                List<CategoryDto> categories = new ArrayList<CategoryDto>();
-                int technologyCategoriesCount = 0;
-                for (Category category: offer.getCategories()) {
-                    technologyCategoriesCount++;
-                    categories.add(
-                        new CategoryDto(
-                            category.getId(),
-                            category.getName(),
-                            technologyCategoriesCount
-                        )
-                    );
-                }
-                technologiesWithCategories.put(offer.getTechnology(), categories);
+                technologiesWithCategories.put(offer.getTechnology(), createInitialCategories(offer.getCategories()));
             } else {
-                for (Category category: offer.getCategories()) {
-                    CategoryDto alreadyAddedCategory = null;
-                    Integer alreadyAddedDtoIndex = null;
-                    int technologyCategoriesCount = 0;
-                    int index = -1;
-
-                    for (CategoryDto technologyCategory : technologyWithCategories) {
-                        index++;
-                        technologyCategoriesCount++;
-                        if (null == alreadyAddedCategory && technologyCategory.getCategoryName().equals(category.getName()) /* TODO think about switch to compare Category Id */) {
-                            alreadyAddedCategory = technologyCategory;
-                            alreadyAddedDtoIndex = index;
-                        }
-                    }
-
-                    if (null == alreadyAddedCategory) {
-                        technologyWithCategories.add(
-                            new CategoryDto(
-                                category.getId(),
-                                category.getName(),
-                                technologyCategoriesCount
-                            )
-                        );
-                    } else {
-                        technologyWithCategories.remove(alreadyAddedDtoIndex.intValue());
-                        technologyWithCategories.add(alreadyAddedCategory.withAddedOccurrence(technologyCategoriesCount));
-                    }
-                }
+                updateExistingTechnologyCategories(offer.getCategories(), technologyWithCategories);
             }
 
-            for (String technology : technologiesWithCategories.keySet()) {
-                List<CategoryDto> technologyCategories = technologiesWithCategories.get(technology);
-                int totalCategoriesCount = 0;
-                for (CategoryDto categoryDto : technologyCategories) {
-                    totalCategoriesCount += categoryDto.getCount();
-                }
-                List<CategoryDto> recalculatedCategories = new ArrayList<CategoryDto>();
-                for (CategoryDto categoryDto : technologyCategories) {
-                    recalculatedCategories.add(categoryDto.withRecalculatedPercentage(totalCategoriesCount));
-                }
-
-                recalculatedCategories.sort(new Comparator<CategoryDto>() {
-                    @Override
-                    public int compare(CategoryDto dto1, CategoryDto dto2) {
-                        return dto2.getCount().compareTo(dto1.getCount());
-                    }
-                });
-
-                technologiesWithCategories.put(technology, recalculatedCategories);
-            }
+            sort(technologiesWithCategories);
         }
 
         return new CategoriesStatisticsDto(
             new FiltersDto(),
             new CategoriesDto(technologiesWithCategories)
         );
+    }
+
+    public static List<CategoryDto> createInitialCategories(Set<Category> offerCategories) {
+        List<CategoryDto> categories = new ArrayList<CategoryDto>();
+        int technologyCategoriesCount = 0;
+        for (Category category: offerCategories) {
+            technologyCategoriesCount++;
+            categories.add(
+                new CategoryDto(
+                    category.getId(),
+                    category.getName(),
+                    technologyCategoriesCount
+                )
+            );
+        }
+        return categories;
+    }
+
+    public static void updateExistingTechnologyCategories(
+        Set<Category> offerCategories,
+        List<CategoryDto> technologyWithCategories
+    ) {
+        for (Category category: offerCategories ) {
+            CategoryDto alreadyAddedCategory = null;
+            Integer alreadyAddedDtoIndex = null;
+            int technologyCategoriesCount = 0;
+            int index = -1;
+
+            for (CategoryDto technologyCategory : technologyWithCategories) {
+                index++;
+                technologyCategoriesCount++;
+                if (null == alreadyAddedCategory && technologyCategory.getCategoryName().equals(category.getName()) /* TODO think about switch to compare Category Id */) {
+                    alreadyAddedCategory = technologyCategory;
+                    alreadyAddedDtoIndex = index;
+                }
+            }
+
+            if (null == alreadyAddedCategory) {
+                technologyWithCategories.add(
+                    new CategoryDto(
+                        category.getId(),
+                        category.getName(),
+                        technologyCategoriesCount
+                    )
+                );
+            } else {
+                technologyWithCategories.remove(alreadyAddedDtoIndex.intValue());
+                technologyWithCategories.add(alreadyAddedCategory.withAddedOccurrence(technologyCategoriesCount));
+            }
+        }
+    }
+
+    public static void sort(HashMap<String, List<CategoryDto>> technologiesWithCategories) {
+        for (String technology : technologiesWithCategories.keySet()) {
+            List<CategoryDto> technologyCategories = technologiesWithCategories.get(technology);
+            int totalCategoriesCount = 0;
+            for (CategoryDto categoryDto : technologyCategories) {
+                totalCategoriesCount += categoryDto.getCount();
+            }
+            List<CategoryDto> recalculatedCategories = new ArrayList<CategoryDto>();
+            for (CategoryDto categoryDto : technologyCategories) {
+                recalculatedCategories.add(categoryDto.withRecalculatedPercentage(totalCategoriesCount));
+            }
+
+            recalculatedCategories.sort(new Comparator<CategoryDto>() {
+                @Override
+                public int compare(CategoryDto dto1, CategoryDto dto2) {
+                    return dto2.getCount().compareTo(dto1.getCount());
+                }
+            });
+
+            technologiesWithCategories.put(technology, recalculatedCategories);
+        }
     }
 }
