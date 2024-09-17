5a87e31fcee5b8bc141cf0273f072e54cea4632b
RobertCzaja <robertczaja.dev@gmail.com>
2024-09-17 18:10:33 +0200

    Add unit test for report

--- src/test/java/pl/api/itoffers/unit/offer/ReportTest.java
+++ src/test/java/pl/api/itoffers/unit/offer/ReportTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.api.itoffers.helper.OfferBuilder;
 import pl.api.itoffers.offer.application.dto.CategoriesStatisticsDto;
+import pl.api.itoffers.offer.application.dto.CategoryDto;
 import pl.api.itoffers.offer.application.service.ReportService;
 import pl.api.itoffers.offer.domain.Offer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,17 +27,14 @@ public class ReportTest {
         this.reportService = new ReportService(this.offerRepository);
     }
 
-    /*
-     *  TODO use DataProvider
-     *  TODO under the development
-     */
+    /* TODO to refactor - it's ugly! */
     @Test
     public void shouldReturnExpectedStatistics() {
 
         List<Offer> offers = new ArrayList<>();
         offers.add(builder.job("php").skills("php", "mysql", "docker").build());
         offers.add(builder.job("php").skills("php", "docker").build());
-        offers.add(builder.job("php").skills("php 8.2", "kubernetes").build());
+        offers.add(builder.job("php").skills("php", "kubernetes").build());
         offers.add(builder.job("php").skills("php", "mysql").build());
         offers.add(builder.job("php").skills("php", "postgres", "phpunit", "ci/cd").build());
         offers.add(builder.job("java").skills("java", "postgres", "maven").build());
@@ -46,7 +45,43 @@ public class ReportTest {
 
         CategoriesStatisticsDto dto = reportService.computeCategoriesStatistics(null, null);
 
-        assertThat("").isNotNull(); // todo assertion to change
-    }
+        assertThat(dto.getResult().getList().get("java").size()).isEqualTo(4);
+        assertThat(dto.getResult().getList().get("php").size()).isEqualTo(7);
 
+
+        HashMap<String, HashMap<Double, Integer>> expected = new HashMap<String, HashMap<Double, Integer>>();
+
+        List<List> java = List.of(
+            List.of("java", 40.0, 4),
+            List.of("postgres", 30.0, 3),
+            List.of("hibernate", 20.0, 2),
+            List.of("maven", 10.0, 1)
+        );
+        List<List> php = List.of(
+            List.of("php", 38.46153846153846, 5),
+            List.of("docker", 15.384615384615385, 2),
+            List.of("mysql", 15.384615384615385, 2),
+            List.of("kubernetes", 7.6923076923076925, 1),
+            List.of("phpunit", 7.6923076923076925, 1),
+            List.of("postgres", 7.6923076923076925, 1),
+            List.of("ci/cd", 7.6923076923076925, 1)
+        );
+
+        for (int i = 0; i <= 3; i++) {
+            CategoryDto categoryDto = dto.getResult().getList().get("java").get(i);
+            List expectedCategory = java.get(i);
+            assertThat(categoryDto.getCategoryName()).isEqualTo(expectedCategory.get(0));
+            assertThat(categoryDto.getPercentage()).isEqualTo(expectedCategory.get(1));
+            assertThat(categoryDto.getCount()).isEqualTo(expectedCategory.get(2));
+        }
+
+
+        for (int i = 0; i <= 3; i++) {
+            CategoryDto categoryDto = dto.getResult().getList().get("php").get(i);
+            List expectedCategory = php.get(i);
+            assertThat(categoryDto.getCategoryName()).isEqualTo(expectedCategory.get(0));
+            assertThat(categoryDto.getPercentage()).isEqualTo(expectedCategory.get(1));
+            assertThat(categoryDto.getCount()).isEqualTo(expectedCategory.get(2));
+        }
+    }
 }
