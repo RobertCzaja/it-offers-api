a2ea09f83f3787353383bddbf7ba6b607ec0f1e2
RobertCzaja <robertczaja.dev@gmail.com>
2024-09-19 07:11:01 +0200

    Move calling mechanism to separate class

--- src/test/java/pl/api/itoffers/integration/offer/ReportEndpointValidationITest.java
+++ src/test/java/pl/api/itoffers/integration/offer/ReportEndpointValidationITest.java
@@ -3,28 +3,16 @@ package pl.api.itoffers.integration.offer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.util.UriComponentsBuilder;
 import pl.api.itoffers.helper.AbstractITest;
-import pl.api.itoffers.helper.ApiAuthorizationHelper;
-import pl.api.itoffers.helper.AuthorizationCredentials;
-import pl.api.itoffers.offer.application.dto.CategoriesStatisticsDto;
-import pl.api.itoffers.offer.ui.controller.ReportController;
-
-import java.net.URI;
+import pl.api.itoffers.integration.offer.helper.ReportEndpointCaller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReportEndpointValidationITest extends AbstractITest  {
-
     @Autowired
-    private TestRestTemplate template;
-    @Autowired
-    private ApiAuthorizationHelper apiAuthorizationHelper;
+    private ReportEndpointCaller reportEndpointCaller;
 
     @BeforeEach
     public void setUp() {
@@ -33,7 +21,7 @@ public class ReportEndpointValidationITest extends AbstractITest  {
 
     @Test
     public void shouldReturnErrorResponseOnInvalidDatesRange() {
-        ResponseEntity<String> response = makeRequest("2024-09-01", "2024-08-01");
+        ResponseEntity<String> response = reportEndpointCaller.makeRequest("2024-09-01", "2024-08-01");
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         assertThat(response.getBody()).contains("dateFrom cannot be greater thant dateTo");
@@ -41,12 +29,7 @@ public class ReportEndpointValidationITest extends AbstractITest  {
 
     @Test
     public void shouldUserDifferentThanAdminIsNotAllowToGetCategoriesReport() {
-        ResponseEntity<String> response = template.exchange(
-                ReportController.PATH_CATEGORY,
-                HttpMethod.GET,
-                new HttpEntity<>(apiAuthorizationHelper.getHeaders(AuthorizationCredentials.USER)),
-                String.class
-        );
+        ResponseEntity<String> response = reportEndpointCaller.makeRequestAsUser();
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
         assertThat(response.getBody()).contains("Access denied");
@@ -54,30 +37,9 @@ public class ReportEndpointValidationITest extends AbstractITest  {
 
     @Test
     public void shouldNotAllowToPassMadeUpDateFrom() {
-        ResponseEntity<String> response = makeRequest("2026-01-01", null);
+        ResponseEntity<String> response = reportEndpointCaller.makeRequest("2026-01-01", null);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
         assertThat(response.getBody()).contains("dateFrom cannot be greater thant dateTo");
-    }
-
-    private ResponseEntity<String> makeRequest(String dateFrom, String dateTo) {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(ReportController.PATH_CATEGORY);
-
-        if (null != dateFrom) {
-            builder = builder.queryParam("dateFrom", dateFrom);
-        }
-
-        if (null != dateTo) {
-            builder = builder.queryParam("dateTo", dateTo);
-        }
-
-        URI uri = builder.build().toUri();
-
-        return template.exchange(
-            uri,
-            HttpMethod.GET,
-            new HttpEntity<>(apiAuthorizationHelper.getHeaders(AuthorizationCredentials.ADMIN)),
-            String.class
-        );
     }
 }
---
+++ src/test/java/pl/api/itoffers/integration/offer/helper/ReportEndpointCaller.java
@@ -0,0 +1,53 @@
+package pl.api.itoffers.integration.offer.helper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+import pl.api.itoffers.helper.ApiAuthorizationHelper;
+import pl.api.itoffers.helper.AuthorizationCredentials;
+import pl.api.itoffers.offer.ui.controller.ReportController;
+
+import java.net.URI;
+
+@Service
+public class ReportEndpointCaller {
+
+    @Autowired
+    private TestRestTemplate template;
+    @Autowired
+    private ApiAuthorizationHelper apiAuthorizationHelper;
+
+    public ResponseEntity<String> makeRequest(String dateFrom, String dateTo) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(ReportController.PATH_CATEGORY);
+
+        if (null != dateFrom) {
+            builder = builder.queryParam("dateFrom", dateFrom);
+        }
+
+        if (null != dateTo) {
+            builder = builder.queryParam("dateTo", dateTo);
+        }
+
+        URI uri = builder.build().toUri();
+
+        return template.exchange(
+            uri,
+            HttpMethod.GET,
+            new HttpEntity<>(apiAuthorizationHelper.getHeaders(AuthorizationCredentials.ADMIN)),
+            String.class
+        );
+    }
+
+    public ResponseEntity<String> makeRequestAsUser() {
+        return template.exchange(
+            ReportController.PATH_CATEGORY,
+            HttpMethod.GET,
+            new HttpEntity<>(apiAuthorizationHelper.getHeaders(AuthorizationCredentials.USER)),
+            String.class
+        );
+    }
+}
