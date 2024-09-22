d6a22c5c45b94acf4222732d337097780f754727
RobertCzaja <robertczaja.dev@gmail.com>
2024-09-22 09:04:57 +0200

    Move Offer Categories assertions to dedicated class

---
+++ src/test/java/pl/api/itoffers/helper/assertions/OfferCategoriesAssert.java
@@ -0,0 +1,26 @@
+package pl.api.itoffers.helper.assertions;
+
+import pl.api.itoffers.offer.application.dto.CategoriesStatisticsDto;
+import pl.api.itoffers.offer.application.dto.CategoryDto;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OfferCategoriesAssert {
+
+    public static void hasExactCategories(
+        String technologyName,
+        CategoriesStatisticsDto dto,
+        List<List> expectedCategories
+    ) {
+        assertThat(dto.getResult().getList().get(technologyName).size()).isEqualTo(expectedCategories.size());
+        for (int i = 0; i <= (expectedCategories.size() - 1); i++) {
+            CategoryDto categoryDto = dto.getResult().getList().get(technologyName).get(i);
+            List expectedCategory = expectedCategories.get(i);
+            assertThat(categoryDto.getCategoryName()).isEqualTo(expectedCategory.get(0));
+            assertThat(categoryDto.getPercentage()).isEqualTo(expectedCategory.get(1));
+            assertThat(categoryDto.getCount()).isEqualTo(expectedCategory.get(2));
+        }
+    }
+}
--- src/test/java/pl/api/itoffers/unit/offer/ReportTest.java
+++ src/test/java/pl/api/itoffers/unit/offer/ReportTest.java
@@ -3,16 +3,13 @@ package pl.api.itoffers.unit.offer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.api.itoffers.helper.OfferBuilder;
+import pl.api.itoffers.helper.assertions.OfferCategoriesAssert;
 import pl.api.itoffers.offer.application.dto.CategoriesStatisticsDto;
-import pl.api.itoffers.offer.application.dto.CategoryDto;
 import pl.api.itoffers.offer.application.service.ReportService;
 import pl.api.itoffers.offer.domain.Offer;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReportTest {
 
@@ -27,16 +24,15 @@ public class ReportTest {
         this.reportService = new ReportService(this.offerRepository);
     }
 
-    /* TODO to refactor - it's ugly! */
+    /* TODO Bug https://github.com/RobertCzaja/it-offers/issues/41 */
     @Test
     public void shouldReturnExpectedStatistics() {
-
         List<Offer> offers = new ArrayList<>();
-        offers.add(builder.job("php").skills("php", "mysql", "docker").build());
-        offers.add(builder.job("php").skills("php", "docker").build());
-        offers.add(builder.job("php").skills("php", "kubernetes").build());
         offers.add(builder.job("php").skills("php", "mysql").build());
-        offers.add(builder.job("php").skills("php", "postgres", "phpunit", "ci/cd").build());
+        offers.add(builder.job("php").skills("php", "docker").build());
+        offers.add(builder.job("php").skills("php", "kubernetes", "docker").build());
+        offers.add(builder.job("php").skills("php", "mysql").build());
+        offers.add(builder.job("php").skills("php", "docker").build());
         offers.add(builder.job("java").skills("java", "postgres", "maven").build());
         offers.add(builder.job("java").skills("java", "postgres").build());
         offers.add(builder.job("java").skills("java", "hibernate").build());
@@ -45,43 +41,17 @@ public class ReportTest {
 
         CategoriesStatisticsDto dto = reportService.computeCategoriesStatistics(null, null, null);
 
-        assertThat(dto.getResult().getList().get("java").size()).isEqualTo(4);
-        assertThat(dto.getResult().getList().get("php").size()).isEqualTo(7);
-
-
-        HashMap<String, HashMap<Double, Integer>> expected = new HashMap<String, HashMap<Double, Integer>>();
-
-        List<List> java = List.of(
+        OfferCategoriesAssert.hasExactCategories("java", dto, List.of(
             List.of("java", 40.0, 4),
             List.of("postgres", 30.0, 3),
             List.of("hibernate", 20.0, 2),
             List.of("maven", 10.0, 1)
-        );
-        List<List> php = List.of(
-            List.of("php", 38.46153846153846, 5),
-            List.of("docker", 15.384615384615385, 2),
-            List.of("mysql", 15.384615384615385, 2),
-            List.of("kubernetes", 7.6923076923076925, 1),
-            List.of("phpunit", 7.6923076923076925, 1),
-            List.of("postgres", 7.6923076923076925, 1),
-            List.of("ci/cd", 7.6923076923076925, 1)
-        );
-
-        for (int i = 0; i <= 3; i++) {
-            CategoryDto categoryDto = dto.getResult().getList().get("java").get(i);
-            List expectedCategory = java.get(i);
-            assertThat(categoryDto.getCategoryName()).isEqualTo(expectedCategory.get(0));
-            assertThat(categoryDto.getPercentage()).isEqualTo(expectedCategory.get(1));
-            assertThat(categoryDto.getCount()).isEqualTo(expectedCategory.get(2));
-        }
-
-
-        for (int i = 0; i <= 3; i++) {
-            CategoryDto categoryDto = dto.getResult().getList().get("php").get(i);
-            List expectedCategory = php.get(i);
-            assertThat(categoryDto.getCategoryName()).isEqualTo(expectedCategory.get(0));
-            assertThat(categoryDto.getPercentage()).isEqualTo(expectedCategory.get(1));
-            assertThat(categoryDto.getCount()).isEqualTo(expectedCategory.get(2));
-        }
+        ));
+        OfferCategoriesAssert.hasExactCategories("php", dto, List.of(
+            List.of("php", 45.45454545454545, 5),
+            List.of("docker", 27.272727272727273, 3),
+            List.of("mysql", 18.181818181818183, 2),
+            List.of("kubernetes", 9.090909090909092, 1)
+        ));
     }
 }
