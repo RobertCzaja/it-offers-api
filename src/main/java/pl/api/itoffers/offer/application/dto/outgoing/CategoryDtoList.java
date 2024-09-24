797c6461543d35641895828661c5a8f71fdc2868
RobertCzaja <robertczaja.dev@gmail.com>
2024-09-24 19:49:31 +0200

    Prepare new version of Technology Categories List class

--- src/main/java/pl/api/itoffers/offer/application/dto/outgoing/CategoryDtoList.java
+++ src/main/java/pl/api/itoffers/offer/application/dto/outgoing/CategoryDtoList.java
@@ -1,5 +1,6 @@
 package pl.api.itoffers.offer.application.dto.outgoing;
 
+import pl.api.itoffers.offer.application.collection.TechnologyCategories;
 import pl.api.itoffers.offer.domain.Category;
 
 import java.util.ArrayList;
@@ -7,6 +8,11 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * @deprecated use rather:
+ * @see TechnologyCategories
+ * TODO that class needs to be removed
+ */
 public class CategoryDtoList {
     public static List<CategoryDto> create(Set<Category> offerCategories) {
         List<CategoryDto> categories = new ArrayList<CategoryDto>();
