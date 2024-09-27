869223f743df4401fad9879724bc6065e0d796ba
RobertCzaja <robertczaja.dev@gmail.com>
2024-09-27 08:45:12 +0200

    Dead end CLI to create new Structure for Salaries based on Mongodb documents

--- src/main/java/pl/api/itoffers/offer/ui/cli/MigrateOfferSalariesToNewFormatCli.java
+++ src/main/java/pl/api/itoffers/offer/ui/cli/MigrateOfferSalariesToNewFormatCli.java
@@ -14,12 +14,14 @@ import pl.api.itoffers.provider.justjoinit.JustJoinItRepository;
 import pl.api.itoffers.provider.justjoinit.model.JustJoinItRawOffer;
 import software.amazon.awssdk.services.secretsmanager.endpoints.internal.Value;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
- * OneUse
+ * OneUse - not finished!
+ * @deprecated it's not finished - that solution with migrating Salaries it's a dead end because there is
+ * a problem with matching RawOffers from MongoDB and with that Offers from Postgres. For one Postgres Offer
+ * there can be multiple RawOffers, and sometimes you just fetch for incorrect MongoDB documents for current Postgres record.
  */
 @Slf4j
 @ShellComponent
@@ -32,8 +34,7 @@ public class MigrateOfferSalariesToNewFormatCli {
     @Autowired
     private JustJoinItRepository justJoinItRepository;
 
-    /*todo change name to: migrate-offer-salaries-to-new-format*/
-    @ShellMethod(key="m")
+    @ShellMethod(key="migrate-offer-salaries-to-new-format")
     public String migrate(String mode) {
 
         List<Offer> offers = offerRepository.findAll();
@@ -50,7 +51,69 @@ public class MigrateOfferSalariesToNewFormatCli {
                 throw new RuntimeException(String.format("Raw data does not exist for OfferId %s", offer.getId()));
             }
 
-            // todo check if all offers from collection are the same
+            for (JustJoinItRawOffer rawOffer : rawOffers) {
+                Map<String, Object> first = rawOffers.get(0).getOffer();
+                Map<String, Object> second = rawOffer.getOffer();
+
+                ArrayList<LinkedHashMap> employmentTypes1 = (ArrayList<LinkedHashMap>) first.get("employmentTypes");
+                ArrayList<LinkedHashMap> employmentTypes2 = (ArrayList<LinkedHashMap>) second.get("employmentTypes");
+
+                Integer employmentTypes1To = (Integer) employmentTypes1.get(0).get("to");
+                Integer employmentTypes1From = (Integer) employmentTypes1.get(0).get("from");
+                String employmentTypes1Currency = (String) employmentTypes1.get(0).get("currency");
+                String employmentTypes1Type = (String) employmentTypes1.get(0).get("type");
+
+                Integer employmentTypes2To = (Integer) employmentTypes2.get(0).get("to");
+                Integer employmentTypes2From = (Integer) employmentTypes2.get(0).get("from");
+                String employmentTypes2Currency = (String) employmentTypes2.get(0).get("currency");
+                String employmentTypes2Type = (String) employmentTypes2.get(0).get("type");
+
+                if (null == employmentTypes1To && null == employmentTypes2To) {
+                    continue;
+                }
+
+                if (!employmentTypes1To.equals(employmentTypes2To)) {
+                    throw new RuntimeException(String.format("Salaries to are different for OfferId %s", offer.getId()));
+                }
+                if (!employmentTypes1From.equals(employmentTypes2From)) {
+                    throw new RuntimeException(String.format("Salaries from are different for OfferId %s", offer.getId()));
+                }
+                if (!employmentTypes1Currency.equals(employmentTypes2Currency)) {
+                    throw new RuntimeException(String.format("Salaries currencies are different for OfferId %s", offer.getId()));
+                }
+                if (!employmentTypes1Type.equals(employmentTypes2Type)) {
+                    throw new RuntimeException(String.format("Employment types are different for OfferId %s", offer.getId()));
+                }
+
+//                for (String key : second.keySet()) {
+//                    if (null == first.get(key) && null == second.get(key)) {
+//                        continue;
+//                    }
+//
+//                    Object firstValue = first.get(key);
+//                    Object secondValue = second.get(key);
+//
+//                    if (firstValue instanceof List<?> || secondValue instanceof List<?>) {
+//                        List<Object> firstValueArray = (List<Object>) firstValue;
+//                        List<Object> secondValueArray = (List<Object>) secondValue;
+//
+//                        if (firstValueArray.size() != secondValueArray.size()) {
+//                            if (!key.equals("multilocation")) {
+//                                throw new RuntimeException(String.format("RawOffers arrays are different for OfferId %s", offer.getId()));
+//                            }
+//                        }
+//                        continue;
+//                    }
+//
+//                    if (key.equals("companyLogoThumbUrl") || key.equals("publishedAt")) {
+//                        continue;
+//                    }
+//
+//                    if (!firstValue.equals(secondValue)) {
+//                        throw new RuntimeException(String.format("RawOffers are different for OfferId %s", offer.getId()));
+//                    }
+//                }
+            }
 
             if (false == mode.equals("migrate")) {
                 continue;
