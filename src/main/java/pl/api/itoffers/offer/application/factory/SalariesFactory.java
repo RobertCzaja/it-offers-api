3f15929a64e1fc38d181ac18adc5046846762e43
RobertCzaja <robertczaja.dev@gmail.com>
2024-09-28 08:09:00 +0200

    WIP SalariesFactory

--- src/main/java/pl/api/itoffers/offer/application/factory/SalariesFactory.java
+++ src/main/java/pl/api/itoffers/offer/application/factory/SalariesFactory.java
@@ -3,6 +3,7 @@ package pl.api.itoffers.offer.application.factory;
 import org.springframework.stereotype.Service;
 import pl.api.itoffers.offer.domain.Offer;
 import pl.api.itoffers.offer.domain.Salary;
+import pl.api.itoffers.offer.domain.SalaryAmount;
 import pl.api.itoffers.provider.justjoinit.model.JustJoinItRawOffer;
 
 import java.util.*;
@@ -18,18 +19,54 @@ public class SalariesFactory {
     public Set<Salary> create(Offer offer, JustJoinItRawOffer rawOffer) {
         // TODO at this stage Offer Entity needs to contain an ID
 
-        List<LinkedHashMap> employmentTypes = (List<LinkedHashMap>) rawOffer
-            .getOffer()
-            .getOrDefault("employmentTypes", new ArrayList<>());
+        Set<Salary> salaries = new HashSet<Salary>();
+        List<LinkedHashMap> employmentTypes = (List<LinkedHashMap>) rawOffer.getOffer().get("employmentTypes");
 
-        if (employmentTypes.isEmpty()) {
+        if (null == employmentTypes) {
             return new HashSet<Salary>();
         }
 
-        for (LinkedHashMap employmentType : employmentTypes) {
-            // todo
-        }
+        employmentTypes.forEach(employmentType -> salaries.addAll(createSalaries(offer.getId(), employmentType)));
 
         return new HashSet<Salary>();
+    }
+
+    public Set<Salary> createSalaries(UUID offerId, LinkedHashMap employmentType) {
+        Set<Salary> salaries = new HashSet<Salary>();
+        Integer to = (Integer) employmentType.get("to");
+        Integer from = (Integer) employmentType.get("from");
+        String currency = (String) employmentType.get("currency");
+        Boolean isPln = currency.equalsIgnoreCase("pln");
+
+        if (null == to || null == from) {
+            return salaries;
+        }
+
+        salaries.add(
+            new Salary(
+                offerId,
+                new SalaryAmount(from, to, currency.toUpperCase()),
+                (String) employmentType.get("type"),
+                true
+            )
+        );
+
+        if (!isPln) {
+            Double plnTo = (Double) employmentType.get("to_pln");
+            Double plnFrom = (Double) employmentType.get("from_pln");
+
+            if (null != plnTo && null != plnFrom) {
+                salaries.add(
+                    new Salary(
+                        offerId,
+                        new SalaryAmount(plnFrom.intValue(), plnTo.intValue(), "PLN"),
+                        (String) employmentType.get("type"),
+                        false
+                    )
+                );
+            }
+        }
+
+        return salaries;
     }
 }
