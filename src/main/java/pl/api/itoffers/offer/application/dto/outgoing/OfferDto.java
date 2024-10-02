fd3f47f30afaddc17607703deed69e1280674408
RobertCzaja <robertczaja.dev@gmail.com>
2024-10-02 21:57:53 +0200

    Remove not necessary annotation

--- src/main/java/pl/api/itoffers/offer/application/dto/outgoing/OfferDto.java
+++ src/main/java/pl/api/itoffers/offer/application/dto/outgoing/OfferDto.java
@@ -1,13 +1,10 @@
 package pl.api.itoffers.offer.application.dto.outgoing;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 @Value
 @RequiredArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class OfferDto {
     Integer amountFrom;
     Integer amountTo;
