ff3a808e89442e5d22e35b7c6a6761eb5c8ac3c6
RobertCzaja <robertczaja.dev@gmail.com>
2024-10-12 14:52:51 +0200

    Remove deprecated columns

---
+++ src/main/resources/db/migration/V4__offer_remove_salary_columns.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "offer" DROP COLUMN salary_currency;
+ALTER TABLE "offer" DROP COLUMN salary_employment_type;
+ALTER TABLE "offer" DROP COLUMN salary_from;
+ALTER TABLE "offer" DROP COLUMN salary_to;
