28a7cb7b0ea31e2bc5771e7578da496f1f50fa64
RobertCzaja <robertczaja.dev@gmail.com>
2024-10-16 19:14:35 +0200

    Add some todos to CLI

--- src/main/java/pl/api/itoffers/provider/justjoinit/ui/cli/ImportJJITOffersFromFileCli.java
+++ src/main/java/pl/api/itoffers/provider/justjoinit/ui/cli/ImportJJITOffersFromFileCli.java
@@ -1,11 +1,17 @@
 package pl.api.itoffers.provider.justjoinit.ui.cli;
 
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+import pl.api.itoffers.shared.aws.AwsS3Connector;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Slf4j
 @ShellComponent
@@ -14,13 +20,37 @@ import org.springframework.shell.standard.ShellOption;
 public class ImportJJITOffersFromFileCli {
     // todo get scrappingId from a client, or generate uuid and show to the client
     // todo check if I can create S3 container in docker compose
-    // todo move bucketname also to envs - maybe also filename should be in the env?
+    // todo create next cli to migrate JJIT MongoDB Offers to Postgres Offer
+    private static final String FILE_NAME = "dev-recruit.just_join_it_raw_offers.json";
+
+    private AwsS3Connector awsS3Connector;
 
     @ShellMethod(key="jjit-s3")
     public void saveInMongoDBJJITOffersStoredInS3(
         @ShellOption(defaultValue = "test") String mode,
         @ShellOption(defaultValue = "1") String limit
-    ) {
+    ) throws IOException {
 
+        // todo add log that fetching file just started
+        String justJoinItOffers = fetchJson();
+        // todo add log that fetching file has been fetched
+        // todo convert that string to JSON object
+
+        // todo add limit
+        // todo check if JJIT raw offer isn't already in MongoDB
+    }
+
+    // todo move to some service
+    private String fetchJson() throws IOException {
+        S3ObjectInputStream inputStream = awsS3Connector.download(FILE_NAME);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        return sb.toString();
     }
 }
--- src/main/java/pl/api/itoffers/shared/aws/AwsS3Connector.java
+++ src/main/java/pl/api/itoffers/shared/aws/AwsS3Connector.java
@@ -1,18 +1,16 @@
 package pl.api.itoffers.shared.aws;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.regions.Region;
 
 @Service
-public class AWSCloudUtil {
+public class AwsS3Connector {
 
     @Value("${application.aws.s3.accessKey}")
     private String accessKey;
@@ -21,21 +19,19 @@ public class AWSCloudUtil {
     @Value("${application.aws.s3.bucket}")
     private String bucket;
 
-    private AWSCredentials awsCredentials(String accessKey, String secretKey) {
-        return new BasicAWSCredentials(accessKey, secretKey);
+    public S3ObjectInputStream download(String fileName) {
+        return awsS3ClientBuilder().getObject(bucket, fileName).getObjectContent();
     }
 
-    private AmazonS3 awsS3ClientBuilder(String accessKey, String secretKey) {
+    private AmazonS3 awsS3ClientBuilder() {
         return AmazonS3ClientBuilder
             .standard()
-            .withCredentials(new AWSStaticCredentialsProvider(awsCredentials(accessKey, secretKey)))
+            .withCredentials(
+                new AWSStaticCredentialsProvider(
+                    new BasicAWSCredentials(accessKey, secretKey)
+                )
+            )
             .withRegion(String.valueOf(Region.EU_NORTH_1))
             .build();
-    }
-
-    public S3ObjectInputStream download(String fileName, String accessKey, String secretKey, String bucket) {
-        AmazonS3 s3client = awsS3ClientBuilder(accessKey, secretKey);
-        S3Object s3object = s3client.getObject(bucket, fileName);
-        return s3object.getObjectContent();
     }
 }
