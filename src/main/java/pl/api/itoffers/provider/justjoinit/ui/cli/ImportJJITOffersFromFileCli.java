c71dd4d0027de0b6eef3ec06c26d65418c9a700e
RobertCzaja <robertczaja.dev@gmail.com>
2024-10-17 18:32:10 +0200

    feat(#18): extract json from file that will be saved as offer in MongoDB

--- src/main/java/pl/api/itoffers/provider/justjoinit/ui/cli/ImportJJITOffersFromFileCli.java
+++ src/main/java/pl/api/itoffers/provider/justjoinit/ui/cli/ImportJJITOffersFromFileCli.java
@@ -1,17 +1,24 @@
 package pl.api.itoffers.provider.justjoinit.ui.cli;
 
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+import pl.api.itoffers.provider.justjoinit.JustJoinItRepository;
+import pl.api.itoffers.provider.justjoinit.model.JustJoinItDateTime;
+import pl.api.itoffers.provider.justjoinit.model.JustJoinItRawOffer;
 import pl.api.itoffers.shared.aws.AwsS3Connector;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 @Slf4j
 @ShellComponent
@@ -21,9 +28,12 @@ public class ImportJJITOffersFromFileCli {
     // todo get scrappingId from a client, or generate uuid and show to the client
     // todo check if I can create S3 container in docker compose
     // todo create next cli to migrate JJIT MongoDB Offers to Postgres Offer
+    // todo once it will be functionally finished - refactor all/create separated services to fetch JSON file
     private static final String FILE_NAME = "dev-recruit.just_join_it_raw_offers.json";
 
     private AwsS3Connector awsS3Connector;
+    private ObjectMapper mapper;
+    private JustJoinItRepository repository;
 
     @ShellMethod(key="jjit-s3")
     public void saveInMongoDBJJITOffersStoredInS3(
@@ -31,18 +41,43 @@ public class ImportJJITOffersFromFileCli {
         @ShellOption(defaultValue = "1") String limit
     ) throws IOException {
 
-        // todo add log that fetching file just started
-        String justJoinItOffers = fetchJson();
-        // todo add log that fetching file has been fetched
-        // todo convert that string to JSON object
+        log.info("Start fetching {}", FILE_NAME);
+        String justJoinItOffers = fetchJson(FILE_NAME);
+        log.info("Fetched");
+        Iterator<JsonNode> offersNode = mapper.readTree(justJoinItOffers).elements();
+        UUID scrappingId = UUID.randomUUID();
+        log.info("ScrappingId: {}", scrappingId);
 
+        /* todo duplication from provider/justjoinit/service/JustJoinItPayloadExtractor.java:39 */
+        ArrayList<Map<String, Object>> offers = new ArrayList<>();
+        while (offersNode.hasNext()) {
+            offers.add(mapper.convertValue(offersNode.next(), new TypeReference<Map<String, Object>>() {}));
+        }
+
+        // todo which technology
+        // todo check in MongoDB it is not already added
+        offers.forEach(offer -> {
+            LinkedHashMap<String, String> rawCreatedAt = (LinkedHashMap<String, String>) offer.get("createdAt");
+
+            String a = "";
+
+            repository.save(
+                new JustJoinItRawOffer(
+                    scrappingId,
+                    (String) offer.get("technology"),
+                    offer,
+                    JustJoinItDateTime.createFrom(rawCreatedAt.get("$date")).value)
+            );
+        });
+
+        // todo should be grouped by slug first?
         // todo add limit
         // todo check if JJIT raw offer isn't already in MongoDB
     }
 
     // todo move to some service
-    private String fetchJson() throws IOException {
-        S3ObjectInputStream inputStream = awsS3Connector.download(FILE_NAME);
+    private String fetchJson(String fileName) throws IOException {
+        S3ObjectInputStream inputStream = awsS3Connector.download(fileName);
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
         StringBuilder sb = new StringBuilder();
