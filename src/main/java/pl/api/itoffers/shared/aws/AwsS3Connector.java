98f7c61979804f77bfed5bf8549bfcae6c4190b6
RobertCzaja <robertczaja.dev@gmail.com>
2024-10-18 18:47:36 +0200

    feat(#18): add more logs to s connector

--- src/main/java/pl/api/itoffers/shared/aws/AwsS3Connector.java
+++ src/main/java/pl/api/itoffers/shared/aws/AwsS3Connector.java
@@ -5,6 +5,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.regions.Region;
@@ -13,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+@Slf4j
 @Service
 public class AwsS3Connector {
 
@@ -24,11 +26,14 @@ public class AwsS3Connector {
     private String bucket;
 
     public String fetchJson(String fileName) throws IOException {
+        log.info("[aws-s3-connector] start fetching");
         S3ObjectInputStream inputStream = this.download(fileName);
+        log.info("[aws-s3-connector] fetched");
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
         StringBuilder sb = new StringBuilder();
 
+        log.info("[aws-s3-connector] start converting the file");
         String line;
         while ((line = reader.readLine()) != null) {
             sb.append(line);
