2ff7922a1362271a0b37063f3cfdf54a2611e96c
RobertCzaja <robertczaja.dev@gmail.com>
2024-11-22 18:59:38 +0100

    feat(#66): set uo testing endpoint for log testing

--- src/main/java/pl/api/itoffers/security/ui/controller/TestController.java
+++ src/main/java/pl/api/itoffers/security/ui/controller/TestController.java
@@ -1,21 +1,27 @@
 package pl.api.itoffers.security.ui.controller;
 
-import org.springframework.context.annotation.Profile;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.api.itoffers.shared.logger.Logger;
 
 @RestController
-@Profile("test")
+@Slf4j
+@RequiredArgsConstructor
 public class TestController {
-
     public final static String PATH = "/test";
+
+    private final Logger logger;
 
     @GetMapping(PATH)
     @PreAuthorize("hasRole('ROLE_USER')")
     public ResponseEntity<String> test() {
+        log.info("standard log");
+        logger.info(this.getClass().getName(), "custom log");
         return new ResponseEntity<>("test", HttpStatus.OK);
     }
 }
