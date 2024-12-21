79219672eff96b3d46315d43b9a5d68589b3cd50
RobertCzaja <robertczaja.dev@gmail.com>
2024-12-21 19:04:21 +0100

    feat(#68): start working on new JJIT parser

--- src/test/java/pl/api/itoffers/integration/provider/justjoinit/payload/JustJoinItParams.java
+++ src/test/java/pl/api/itoffers/integration/provider/justjoinit/payload/JustJoinItParams.java
@@ -8,4 +8,6 @@ public class JustJoinItParams {
     public final static String ALL_LOCATIONS_PAYLOAD_DUPLICATED_1_PATH = MAIN_CATALOG + "allLocationsPhpScenarioDuplicated1.json";
     public final static String ALL_LOCATIONS_PAYLOAD_DUPLICATED_2_PATH = MAIN_CATALOG + "allLocationsPhpScenarioDuplicated2.json";
     public final static String ALL_LOCATIONS_PAYLOAD_DUPLICATED_3_PATH = MAIN_CATALOG + "allLocationsPhpScenarioDuplicated3.json";
+    /** in december 2024 JJIT changed structure of the HTML file under that endpoint so it-offers parser needs to be adjusted */
+    public final static String ALL_LOCATIONS_PAYLOAD_B1_DECEMBER = MAIN_CATALOG + "allLocationsScenarioB1December.html";
 }
