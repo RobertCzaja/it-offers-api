73ddecf2e69aaa306e9771f0c21764397d91eefb
RobertCzaja <robertczaja.dev@gmail.com>
2024-12-21 20:41:30 +0100

    feat(#68): json extractor refactored

--- src/main/java/pl/api/itoffers/provider/justjoinit/service/extractor/v2/JsonFromHtmlExtractor.java
+++ src/main/java/pl/api/itoffers/provider/justjoinit/service/extractor/v2/JsonFromHtmlExtractor.java
@@ -6,17 +6,15 @@ import org.jsoup.select.Elements;
 
 public class JsonFromHtmlExtractor {
 
-    /**
-     * TODO to refactor
-     */
     public String getRawJsonFromHtml(String html) {
         Document responseBody = Jsoup.parse(html);
         Elements bodyChildren = responseBody.select("body").last().children();
-        String lastScriptRawContent = bodyChildren.last().html();
-        int firstBraceIndex = lastScriptRawContent.indexOf('{');
-        String cutBody = lastScriptRawContent.substring(firstBraceIndex);
-        String rawJsonPayload = cutBody.replace("]\\n\"])", "");
-        String cleanedJson = rawJsonPayload.replace("\\\"","\"");
-        return cleanedJson.replace("\\\\\"", "\\\"");
+        String lastScriptTagContent = bodyChildren.last().html();
+        int firstBraceIndex = lastScriptTagContent.indexOf('{');
+        return lastScriptTagContent
+            .substring(firstBraceIndex)
+            .replace("]\\n\"])", "")
+            .replace("\\\"","\"")
+            .replace("\\\\\"", "\\\"");
     }
 }
