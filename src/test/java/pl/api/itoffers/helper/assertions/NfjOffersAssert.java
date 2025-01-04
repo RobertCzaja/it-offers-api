e7d2bd28fc01b49dd2b5ba4c6b825c29dd52c67a
RobertCzaja <robertczaja.dev@gmail.com>
2025-01-04 09:35:39 +0100

    feat(#81): assert details of nfj serialized offer

---
+++ src/test/java/pl/api/itoffers/helper/assertions/NfjOffersAssert.java
@@ -0,0 +1,41 @@
+package pl.api.itoffers.helper.assertions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class NfjOffersAssert {
+
+  public static void expects(ArrayList<Map<String, Object>> nfjOffers, int expectedSize) {
+    assertThat(nfjOffers).hasSize(expectedSize);
+    nfjOffers.forEach(
+        (serializedOffer) -> {
+          assertThat(serializedOffer).containsKey("id");
+          assertThat(serializedOffer).containsKey("name");
+          assertThat(serializedOffer).containsKey("location");
+          assertThat(serializedOffer).containsKey("posted");
+          if (null != serializedOffer.get("renewed")) {
+            assertThat(serializedOffer).containsKey("renewed");
+            assertThat(serializedOffer.get("renewed")).isNotNull();
+          }
+          assertThat(serializedOffer).containsKey("title");
+          assertThat(serializedOffer).containsKey("technology");
+          assertThat(serializedOffer).containsKey("logo");
+          assertThat(serializedOffer).containsKey("category");
+          assertThat(serializedOffer).containsKey("seniority");
+          assertThat(serializedOffer).containsKey("url");
+          assertThat(serializedOffer).containsKey("regions");
+          assertThat(serializedOffer).containsKey("fullyRemote");
+          assertThat(serializedOffer).containsKey("salary");
+          assertThat(serializedOffer).containsKey("flavors");
+          assertThat(serializedOffer).containsKey("topInSearch");
+          assertThat(serializedOffer).containsKey("highlighted");
+          assertThat(serializedOffer).containsKey("help4Ua");
+          assertThat(serializedOffer).containsKey("reference");
+          assertThat(serializedOffer).containsKey("searchBoost");
+          assertThat(serializedOffer).containsKey("onlineInterviewAvailable");
+          assertThat(serializedOffer).containsKey("tiles");
+        });
+  }
+}
--- src/test/java/pl/api/itoffers/unit/provider/nofluffjobs/PayloadFromJsonExtractorTest.java
+++ src/test/java/pl/api/itoffers/unit/provider/nofluffjobs/PayloadFromJsonExtractorTest.java
@@ -1,12 +1,11 @@
 package pl.api.itoffers.unit.provider.nofluffjobs;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 import org.junit.Test;
 import pl.api.itoffers.data.nfj.NoFluffJobsParams;
+import pl.api.itoffers.helper.assertions.NfjOffersAssert;
 import pl.api.itoffers.provider.nofluffjobs.PayloadFromJsonExtractor;
 import pl.api.itoffers.shared.utils.fileManager.FileManager;
 
@@ -21,7 +20,8 @@ public class PayloadFromJsonExtractorTest {
 
     ArrayList<Map<String, Object>> extractedJsonOffers = extractor.extractOffers(listPhpPayload);
 
-    assertThat(extractedJsonOffers).hasSize(20);
-    // todo add custom assert class which will check also each element structure
+    NfjOffersAssert.expects(extractedJsonOffers, 20);
   }
+
+  // todo add case which will check empty payload body
 }
