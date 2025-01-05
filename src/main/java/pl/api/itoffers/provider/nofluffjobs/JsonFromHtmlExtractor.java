6fd5f918f882272debd70707d61576ddd8aa16a5
RobertCzaja <robertczaja.dev@gmail.com>
2025-01-05 11:45:37 +0100

    feat(#81): add html page for test scenario

---
+++ src/main/java/pl/api/itoffers/provider/nofluffjobs/JsonFromHtmlExtractor.java
@@ -0,0 +1,11 @@
+package pl.api.itoffers.provider.nofluffjobs;
+
+import org.jsoup.nodes.Document;
+
+public class JsonFromHtmlExtractor {
+
+  public String getRawJsonFromHtml(Document responseBody) {
+    // todo add implementation
+    return "";
+  }
+}
---
+++ src/test/java/pl/api/itoffers/unit/provider/nofluffjobs/JsonFromHtmlExtractorTest.java
@@ -0,0 +1,16 @@
+package pl.api.itoffers.unit.provider.nofluffjobs;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class JsonFromHtmlExtractorTest {
+
+  @Test
+  void _____________() {
+
+    // todo
+
+    assertThat("").isNotNull();
+  }
+}
