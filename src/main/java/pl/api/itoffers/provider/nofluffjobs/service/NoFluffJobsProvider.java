ca778b94e644c7dfb5d6cb56b2d4105202abc22f
RobertCzaja <robertczaja.dev@gmail.com>
2025-01-10 18:20:00 +0100

    feat(#81): fetch each technology offers in separated thread

--- src/main/java/pl/api/itoffers/provider/nofluffjobs/service/NoFluffJobsProvider.java
+++ src/main/java/pl/api/itoffers/provider/nofluffjobs/service/NoFluffJobsProvider.java
@@ -1,23 +1,16 @@
 package pl.api.itoffers.provider.nofluffjobs.service;
 
 import java.util.Arrays;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import pl.api.itoffers.provider.nofluffjobs.exception.NoFluffJobsException;
-import pl.api.itoffers.provider.nofluffjobs.fetcher.details.NoFluffJobsDetailsProvider;
-import pl.api.itoffers.provider.nofluffjobs.fetcher.list.NoFluffJobsListProvider;
-import pl.api.itoffers.provider.nofluffjobs.repository.NoFluffJobsListOfferRepository;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class NoFluffJobsProvider {
 
-  private final NoFluffJobsListProvider listProvider;
-  private final NoFluffJobsListOfferRepository listOfferRepository;
-  private final NoFluffJobsDetailsProvider detailsProvider;
+  private final NoFluffJobsSingleTechnologyOfferWorker worker;
 
   /** TODO under construction */
   public void fetch() {
@@ -27,26 +20,9 @@ public class NoFluffJobsProvider {
     Arrays.stream(technologies)
         .forEach(
             technology -> {
-              UUID scrapingId = UUID.randomUUID();
-              log.info("[technology] fetching {}", technology);
-              listProvider.fetch(technology, scrapingId);
-
-              listOfferRepository
-                  .findByScrapingIdAndTechnology(scrapingId, technology)
-                  .forEach(
-                      listOffer -> {
-                        String slug = (String) listOffer.getOffer().get("url");
-                        log.info("[{}] {}", technology, slug);
-
-                        try {
-                          detailsProvider.fetch(
-                              slug, listOffer.getScrapingId(), listOffer.getOfferId());
-                        } catch (NoFluffJobsException e) {
-                          log.warn(e.getMessage());
-                        }
-                      });
-
-              log.info("fetched successfully");
+              log.info("[{}] start import", technology);
+              worker.fetchOffers(technology);
+              log.info("[{}] import successfully delegated", technology);
             });
   }
 }
---
+++ src/main/java/pl/api/itoffers/provider/nofluffjobs/service/NoFluffJobsSingleTechnologyOfferWorker.java
@@ -0,0 +1,43 @@
+package pl.api.itoffers.provider.nofluffjobs.service;
+
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import pl.api.itoffers.provider.nofluffjobs.exception.NoFluffJobsException;
+import pl.api.itoffers.provider.nofluffjobs.fetcher.details.NoFluffJobsDetailsProvider;
+import pl.api.itoffers.provider.nofluffjobs.fetcher.list.NoFluffJobsListProvider;
+import pl.api.itoffers.provider.nofluffjobs.repository.NoFluffJobsListOfferRepository;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NoFluffJobsSingleTechnologyOfferWorker {
+  private final NoFluffJobsListOfferRepository listOfferRepository;
+  private final NoFluffJobsDetailsProvider detailsProvider;
+  private final NoFluffJobsListProvider listProvider;
+
+  @Async
+  public void fetchOffers(String technology) {
+    UUID scrapingId = UUID.randomUUID();
+
+    log.info("[technology] fetching offer list: {}", technology);
+    listProvider.fetch(technology, scrapingId);
+    log.info("[technology] successfully fetched offer list: {}", technology);
+
+    listOfferRepository
+        .findByScrapingIdAndTechnology(scrapingId, technology)
+        .forEach(
+            listOffer -> {
+              String slug = (String) listOffer.getOffer().get("url");
+              log.info("[{}] {}", technology, slug);
+
+              try {
+                detailsProvider.fetch(slug, listOffer.getScrapingId(), listOffer.getOfferId());
+              } catch (NoFluffJobsException e) {
+                log.warn(e.getMessage());
+              }
+            });
+  }
+}
