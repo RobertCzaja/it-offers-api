cf92589280c7cc2809aa332f06d87786e7dbfea3
RobertCzaja <robertczaja.dev@gmail.com>
2025-01-20 19:47:49 +0100

    feat(#69): add simple import summary object

---
+++ src/main/java/pl/api/itoffers/report/ImportSummary.java
@@ -0,0 +1,26 @@
+package pl.api.itoffers.report;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImportSummary {
+
+    private final Map<String, Integer> newOffersCounter = new HashMap<>();
+
+    public void newOfferAdded(String technology) {
+        if (newOffersCounter.containsKey(technology)) {
+            var count = (Integer) newOffersCounter.get(technology);
+            newOffersCounter.replace(technology, ++count);
+        } else {
+            newOffersCounter.put(technology, 1);
+        }
+    }
+
+    public String getReport() {
+        var sb = new StringBuilder();
+        newOffersCounter.forEach((technology, counter) -> {
+            sb.append(String.format("%s: %s \n", technology, counter));
+        });
+        return sb.toString();
+    }
+}
---
+++ src/test/java/pl/api/itoffers/unit/report/ImportSummaryTest.java
@@ -0,0 +1,23 @@
+package pl.api.itoffers.unit.report;
+
+import org.junit.jupiter.api.Test;
+import pl.api.itoffers.report.ImportSummary;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ImportSummaryTest {
+
+    @Test
+    void shouldCorrectlyBuildAReport() {
+        var summary = new ImportSummary();
+
+        summary.newOfferAdded("java");
+        summary.newOfferAdded("php");
+        summary.newOfferAdded("java");
+
+        assertThat(summary.getReport()).isEqualTo(
+            "java: 2 \n" +
+            "php: 1 \n"
+        );
+    }
+}
