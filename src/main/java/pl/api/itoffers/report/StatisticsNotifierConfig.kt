27dcfb17db7611661b77044598c81ad1112252a1
RobertCzaja <robertczaja.dev@gmail.com>
2025-01-31 08:39:15 +0100

    fix(#69): add notifiers config

--- src/main/java/pl/api/itoffers/report/StatisticsNotifierConfig.kt
+++ src/main/java/pl/api/itoffers/report/StatisticsNotifierConfig.kt
@@ -2,36 +2,18 @@ package pl.api.itoffers.report
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
-import org.springframework.mail.javamail.JavaMailSender
 import pl.api.itoffers.shared.utils.clock.ClockInterface
 
 @Configuration
 open class StatisticsNotifierConfig {
 
-//    @Bean
-//    open fun inMemoryStatisticsNotifier(): InMemoryStatisticsNotifier {
-//        return InMemoryStatisticsNotifier()
-//    }
-//
-//    @Bean
-//    @Primary
-//    open fun emailStatisticsNotifier(mailSender: JavaMailSender): EmailStatisticsNotifier {
-//        return EmailStatisticsNotifier(mailSender)
-//    }
-//
-//    @Bean
-//    open fun statisticsNotifier(mailSender: JavaMailSender): StatisticsNotifier {
-//        return EmailStatisticsNotifier(mailSender)
-//    }
-
     @Bean
-    open fun testEmailController(emailStatisticsNotifier: EmailStatisticsNotifier): TestEmailController {
-        return TestEmailController(emailStatisticsNotifier);
+    open fun testEmailController(): TestEmailController {
+        return TestEmailController(InMemoryStatisticsNotifier());
     }
 
     @Bean
-    open fun importStatistics(emailStatisticsNotifier: EmailStatisticsNotifier, clock: ClockInterface): ImportStatistics {
-        return ImportStatistics(clock, emailStatisticsNotifier)
+    open fun importStatistics(clock: ClockInterface): ImportStatistics {
+        return ImportStatistics(clock, InMemoryStatisticsNotifier())
     }
 }
