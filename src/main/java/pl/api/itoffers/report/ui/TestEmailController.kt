1be74c94cca6d423766340c2664d1dd512473c80
RobertCzaja <robertczaja.dev@gmail.com>
2025-01-31 18:14:12 +0100

    fix(#69): add notifiers config

--- src/main/java/pl/api/itoffers/report/ui/TestEmailController.kt
+++ src/main/java/pl/api/itoffers/report/ui/TestEmailController.kt
@@ -2,6 +2,7 @@ package pl.api.itoffers.report.ui
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import pl.api.itoffers.report.service.EmailStatisticsNotifier
 
 /**
  * TODO should be removed after the implementation of #69
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TestEmailController (
     //private val statisticsNotifier: StatisticsNotifier
+    private val emailStatisticsNotifier: EmailStatisticsNotifier
 ) {
     @GetMapping("/email")
     fun sendEmail(): String {
