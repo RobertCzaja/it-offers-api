ae4e6a352d5ea25c7f40176be20ca8f5f37e6775
RobertCzaja <robertczaja.dev@gmail.com>
2025-01-31 18:21:03 +0100

    fix(#69): add notifiers config

--- src/main/java/pl/api/itoffers/report/service/SomeService.kt
+++ src/main/java/pl/api/itoffers/report/service/SomeService.kt
@@ -1,7 +1,11 @@
 package pl.api.itoffers.report.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class SomeService {
+
+    @Value("\${application.report.destinationEmail}")
+    private val reportToEmail: String? = null
 }
